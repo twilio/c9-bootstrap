5ece0a5391310ce1a34fddf7b9fb887b2826759f
Jingming Niu <jniu@twilio.com>
2017-09-25 15:14:03 -0700

    Remove dunder checks

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask('bootcamp-app')
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+
+app.run(debug=True, host='0.0.0.0', port=8080)
